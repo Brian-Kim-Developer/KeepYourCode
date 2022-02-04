659cbffc24b8b7f624fb91a92c1e72ec73421a49
Gyeongwon Kim <gyeongwonkim@Gyeongwons-MacBook-Pro.local>
2022-02-04 21:03:41 +0900

    Input bundling

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: 'filecache'
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -35,19 +35,12 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const react = require('react');
-              const reactDOM = require('react-dom');
-              console.log(react, reactDOM);
-            `,
+            contents: inputCode
           };
         }
 
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-        // if it is, return it immediately
         if (cachedResult) {
           return cachedResult;
         }
@@ -60,7 +53,6 @@ export const unpkgPathPlugin = () => {
           resolveDir: new URL('./', request.responseURL).pathname
         }
 
-        // store response in cache
         await fileCache.setItem(args.path, result);
 
         return result;
